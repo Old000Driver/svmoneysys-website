3658d0eedd1feedec5cdd7b7d02d11bfb0309a6d
Gja <9206150+gja_001@user.noreply.gitee.com>
2021-07-06 12:52:14 +0800

    deploy

---
+++ precache-manifest.f2378171111e767888a602c21b8c6458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7101884a471767999d0b",
+    "url": "/svmoneysys-website/css/app.3671505e.css"
+  },
+  {
+    "revision": "31699b4f045482a1ad27c94af3db871a",
+    "url": "/svmoneysys-website/index.html"
+  },
+  {
+    "revision": "7101884a471767999d0b",
+    "url": "/svmoneysys-website/js/app.b3a38f19.js"
+  },
+  {
+    "revision": "6c210117a71a8223e451",
+    "url": "/svmoneysys-website/js/chunk-vendors.18af2ca9.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/svmoneysys-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/svmoneysys-website/robots.txt"
+  }
+]);
